5f3c209f6df11a72e5d18def7f48023425d40d39
Pang-Jiah <pangjiah@foxmail.com>
2022-11-09 10:46:35 +0800

    20221109 vics: velocity_averaged no_inertial

--- information_flow/vicsek.py
+++ information_flow/vicsek.py
@@ -1,8 +1,8 @@
 # vicsek model simulation
 # for testing 
-# authority: Pang Jiahun
+# authority: Pang Jiahuan
 # start time: 2022/11/7
-# last time: 2022/11/8
+# last time: 2022/11/9
 # end time: ~
 
 '''
@@ -10,14 +10,13 @@ version description:
     考虑速度连续,参照[2]的表示方法，现在的编程思路是一种中和状态，既有Theta变量又有速度
 
 相对与上一版改进的说明：
-    加入惯性,周期边界条件,画图变成了箭头
+    取消了惯性，改用速度取平均，放弃arctan函数
 '''
 #考虑：
 #   1. 动画部分要不要放入VICSEK类中
 #   2. 数学计算采用[2]的方式？还是逐个元素进行相应的计算
 #   3. 还没有考虑数据存储的部分
-#   4. 取平均改用速度的方式，角度-》当趋向一致的时候老是抖，无法过度
-
+#   4. 加速度的表示，注意量纲dimension
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -158,55 +157,54 @@ def update(vicsek: VICSEK):
     dy = np.subtract.outer(vicsek.P[:, 1], vicsek.P[:, 1]) 
     distance = np.hypot(dx, dy)
     # periodic boundary
-    Ax = (distance >= 0) * (distance < vicsek.r)  # >=0是包括自己 
-    # Ax += (np.abs(dx) >= np.abs(dy)) * (vicsek.l* distance/np.abs(dx+0.000001) - distance)<vicsek.r # 左右
-    # Ax += (np.abs(dx) < np.abs(dy)) *(vicsek.l* distance/np.abs(dy+0.000001) - distance)<vicsek.r # 上下
+    Ax = (distance >= 0) * (distance <= vicsek.r) # >=0是包括自己 
+
     Ax += (dy > vicsek.l/2) * (np.abs(dx)< vicsek.l/2) * (np.hypot(0-dx,vicsek.l-dy)<vicsek.r)
     Ax += (dy > vicsek.l/2) * (dx< -vicsek.l/2) * (np.hypot(-vicsek.l-dx,vicsek.l-dy)<vicsek.r)
     Ax += (dy > vicsek.l/2) * (dx> vicsek.l/2) * (np.hypot(vicsek.l-dx,vicsek.l-dy)<vicsek.r)
     Ax += (dy < vicsek.l/2) * (dx> vicsek.l/2) * (np.hypot(vicsek.l-dx,0-dy)<vicsek.r)#
     Ax += Ax.T
 
-    di = np.maximum(Ax.sum(axis=1), 1)#.reshape(vicsek.n,1)#是包括自己
+    di = np.maximum(Ax.sum(axis=1), 1) #.reshape(vicsek.n,1)
     Dx = np.diag(di)
-    Lx = Dx-Ax
-    id = np.identity(vicsek.n)
-    # print(di)
-    # print(di.reshape(vicsek.n,1))
-    #noise
-    rng = np.random.default_rng()
-    Noises = rng.random((vicsek.n,1))*vicsek.yeta -vicsek.yeta/2
     
-    # vicsek.Theta = np.matmul((id - np.matmul(np.linalg.inv(Dx),Lx)),vicsek.Theta)
-    K = 0.1
-    Z = np.arctan2((np.matmul(Ax,vicsek.V[:,1]).reshape(vicsek.n,1)/di.reshape(vicsek.n,1)-vicsek.v*np.sin(vicsek.Theta)),((np.matmul(Ax,vicsek.V[:,0]).reshape(vicsek.n,1)/di.reshape(vicsek.n,1)-vicsek.v*np.cos(vicsek.Theta)))) 
-    # vicsek.Theta += K*np.arctan((np.matmul(Ax,vicsek.V[:,1]).reshape(vicsek.n,1)/di.reshape(vicsek.n,1)-vicsek.v*np.sin(vicsek.Theta))/((np.matmul(Ax,vicsek.V[:,0]).reshape(vicsek.n,1)/di.reshape(vicsek.n,1)-vicsek.v*np.cos(vicsek.Theta))+0.00000001)) * vicsek.step
-    vicsek.Theta += K*Z* vicsek.step
+    Lx = Dx-Ax
+    Id = np.identity(vicsek.n)
+    # noise
+    rng = np.random.default_rng()
+    Noises = rng.random((vicsek.n,1))*vicsek.yeta - vicsek.yeta/2
+    
+    vicsek.V = np.matmul((Id - np.matmul(np.linalg.inv(Dx),Lx)),vicsek.V)
+    
     '''
     上面的计算变成代码确实有点复杂，但公式还算简单.很多代码是调整矩阵的格式用的
+    ps: “.” 加上运算符表示按元素进行运算
+    V(t+1) = V(t) - Dx^{-1} * Lx * V(t) = (Id - Dx^{-1}* Lx) * V(t)
     
-    Theta(t+1) = Theta(t) +  K .* d(Theta) .* dt                      ps: “.” 加上运算符表示按元素进行运算, K是一个常量
-                         Dx^{-1}*Ax*Vy(t)-v.*sin(Theta(t))                    
-    d(Theta) = arctan(  —————————————————————————————————————— )         Vx为速度x的分量, Vy同理, epsilon趋于0(防止分母为零)
-                         Dx^{-1}*Ax*Vx(t)-v.*cos(Theta(t))+epsilon
-    
-    因此速度变化就是连续了，不会出现方向太大的突变
-    方向差不多的时候就会抖动,由于角度范围并不是连续的原因，并且无法从-pi相互过渡到pi，所以打算不从不从角度上下手了
     '''
-    # print(Z[1]/np.pi*180/vicsek.step/K)
-    vicsek.Theta += Noises
-    vicsek.V = np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))
-    vicsek.P = vicsek.P + np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))*vicsek.step
+
+    vicsek.V += Noises
+    
+    # speed remains unchanged
+    velocityValue = np.hypot(vicsek.V[:,0],vicsek.V[:,1])
+    vicsek.V[:,0] *= vicsek.v/velocityValue
+    vicsek.V[:,1] *= vicsek.v/velocityValue
+    vicsek.Theta = np.arccos(vicsek.V[:,0]/velocityValue)
+
+    vicsek.P  = vicsek.P + vicsek.V * vicsek.step 
+    # print(vicsek.P)
+    # vicsek.V = np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))
+    # vicsek.P = vicsek.P + np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))*vicsek.step
     # vicsek.P = vicsek.P % vicsek.l#取余
     
-    vicsek.P = np.mod(vicsek.P, vicsek.l)#取余
+    vicsek.P = np.mod(vicsek.P, vicsek.l) # 取余
     return vicsek.P
 
 
 #%%
 if __name__ == "__main__":
     #修改参数就修改类的参数就行，这样可以同时跑多个参数
-    vicsek = VICSEK(sizeOfArena= 80.0, number= 200, speed=5, senseRadiu= 10, noises=0.5)
+    vicsek = VICSEK(sizeOfArena= 200.0, number= 400, speed=10, senseRadiu=5, noises=0.6)
     animate(vicsek)
 
 

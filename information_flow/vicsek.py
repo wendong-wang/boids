c33a75362aa3918eecaa0dcd93d174672d1baa5a
Pang-Jiah <pangjiah@foxmail.com>
2022-11-11 22:18:40 +0800

    20221111 vics: structure_changed data_produced README

--- information_flow/vicsek.py
+++ information_flow/vicsek.py
@@ -1,27 +1,34 @@
 # vicsek model simulation
-# for testing 
+# Under developing
 # authority: Pang Jiahuan
 # start time: 2022/11/7
-# last time: 2022/11/9
+# last time: 2022/11/11
 # end time: ~
 
 '''
 version description:
-    考虑速度连续,参照[2]的表示方法，现在的编程思路是一种中和状态，既有Theta变量又有速度
+    改成[3]中的模型，描述参考[2]，加入数据处理部分
 
 相对与上一版改进的说明：
-    取消了惯性，改用速度取平均，放弃arctan函数
+    取消了惯性，改用速度取平均,加入数据保存,但是这里保存只是考虑了角度值
+    结构也大改，将仿真作为类的一个方法，并将仿真数据与模型参数数据进行隔离
 '''
 #考虑：
 #   1. 动画部分要不要放入VICSEK类中
 #   2. 数学计算采用[2]的方式？还是逐个元素进行相应的计算
-#   3. 还没有考虑数据存储的部分
-#   4. 加速度的表示，注意量纲dimension
+#   3. 加速度的表示，注意量纲dimension
+#   4. 各种区间边界的开闭确定： 角度， 噪声
+#   5. HDF5数据的结构
+#   7. 能否把控时间，而不只是仅仅考虑多少步，simulationTime step（暂时删除）存在的意义
 
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
+import time
+import os
+import h5py
+import progressbar
 
 class VICSEK():
     '''
@@ -29,38 +36,44 @@ class VICSEK():
 
     Parameters
     --
-        sizeOfArena: float ( default: )
+    >>> sizeOfArena: float ( default: )
             the linear size of the squrae shape cell where simulations are carried out. 
-        number: int  ( default: )
+    >>> number: int  ( default: )
             the number of units. 
-        speed: float ( default: )
+    >>> speed: float ( default: )
             speed of the units(constant temporarily).
-        senseRadiu: float ( default: )
+    >>> senseRadiu: float ( default: )
             the radius that an unit can feel.
-        noises: float ( default: )
+    >>> noises: float ( default: )
             the strength of noises
-    
-    > hello
+    >>> name: str (default: vicsek)
+            name of viscek model.
+
+    >>> hello vicsek
 
     '''
     '''
     Notation
     --
-    # l: the simulations are carried out in a square shape cell of linear size L 1x1 场地大小
-    # n: n units 1x1 个数
-    # v: value of the velocities of the units  1x1 速度大小
-    # Theta: directions nx1 角度(每个unit运动的不同)
-    # V: velocities of the units nx2 
-    # P: matrix of the positions of the units nx2 位置矩阵
-    # r: the radius that an unit can feel 1x1 所感知的半径
-    # yeta: the strength of the noises 1x1 噪声
+        # l: the simulations are carried out in a square shape cell of linear size L 1x1 场地大小
+        # n: n units 1x1 个数
+        # v: value of the velocities of the units  1x1 速度大小
+        # Theta: directions nx1 角度(每个unit运动的不同)
+        # V: velocities of the units nx2
+        # P: matrix of the positions of the units n x 2 x stepNum 位置矩阵
+        # r: the radius that an unit can feel 1x1 所感知的半径
+        # yeta: the strength of the noises 1x1 噪声
     大小写规范问题:小写为数值，大写为矩阵
+
     '''
-    def __init__(self,sizeOfArena: float, number: int, speed: float, senseRadiu: float,noises: float) -> None:
+    def __init__(self, sizeOfArena: float, number: int, speed: float, senseRadiu: float,noises: float, name: str = "vicsek" ) -> None:
+        self.name = name
+        print(name," is born")
+        
+        #####
         self.parameters_Init(sizeOfArena,number,speed,senseRadiu,noises) #init parameters
-        print("hello world")
-        self.simulationTime = 10 # the duration of the simulation, unit: second
-        self.step = 0.1 # the duration of the step
+        
+
         pass
     
     def parameters_Init(self, sizeOfArena: float, number:int, speed: float, senseRadiu: float, noises: float) -> None:
@@ -69,147 +82,221 @@ class VICSEK():
 
         Parameters
         --
-            sizeOfArena: float ( default: )
+        >>> sizeOfArena: float ( default: )
                 the linear size of the squrae shape cell where simulations are carried out. 
-            number: int  ( default: )
+        >>> number: int  ( default: )
                 the number of units. 
-            speed: float ( default: )
+        >>> speed: float ( default: )
                 speed of the units(constant temporarily).
-            senseRadiu: float ( default: )
+        >>> senseRadiu: float ( default: )
                 the radius that an unit can feel.
-            noises: float ( default: )
-                the strength of noises
+        >>> noises: float ( default: )
+                the strength of noises.
+
         '''
         self.l = sizeOfArena
         self.n = number
         self.v = speed
         self.r = senseRadiu
         self.yeta = noises
-        # init P and theta
+        # init Position P
         rng = np.random.default_rng()
         self.P = rng.random((self.n,2))*self.l
-        # init Theta
-        self.Theta = rng.random((self.n,1))*2*np.pi-np.pi
-        # init Velocity
+        # init angle   Theta        0~2*pi but what is 
+        self.Theta = rng.random((self.n,1))*2*np.pi 
+        # init Velocity  V    
         self.V = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
-        
 
+        
+        pass
 
 
 #================== animation part
-#%%
-def animate(vicsek) -> None:
-    '''
-    start the simulation
-    
-    Parameters:
-        vicsek: VICSEK(class) default: 
-            vicsek model
 
-    '''
-    # print(vicsek)
-    # print(id(vicsek))
-    fig = plt.figure()
-    # ax = fig.subplots()
-    # process, = ax.plot(vicsek.P[:,0],vicsek.P[:,1], c='k', marker ='.', ls='None')
-    plt.quiver(vicsek.P[:,0],vicsek.P[:,1],vicsek.V[:,0],vicsek.V[:,1])
+    def start(self) -> None:
+        '''
+        start the simulation
+        
+        Parameters:
+        '''
+        self.simulation_Init() # init file and folder 
 
-    ani = animation.FuncAnimation(fig=fig, func=_move, fargs= (vicsek, ), frames=100, interval=20, blit=False)
-    plt.xlim((0, vicsek.l))
-    plt.ylim((0, vicsek.l))
-    plt.show()
+        fig = plt.figure() # 
+        plt.quiver(self.P[:,0],self.P[:,1],self.V[:,0],self.V[:,1])
+        ani = animation.FuncAnimation(fig=fig, func=self._move, frames=self.stepNum-1, interval=20, blit=False, repeat=False) # frams-1是因为frame会传两个参数0
+        plt.xlim((0, self.l))
+        plt.ylim((0, self.l))
+        ani.save('./sin_dot.gif', fps=20)
+        self.pbar.finish()
+        print(":) \"Video is saved successfully.\"",self.name,"said")
+        self.data_Save()
+        print(":) \"Data is saved successfully.\"",self.name,"said")
+        # plt.show()
 
-def _move(frameNumber, vicsek: VICSEK):
-    '''
-    # WARN!!!!!!!!!!
-    do not touch easily
-    由动画函数自动调用
+    def simulation_Init(self):
+        '''
+        Init things which are used for simulation
 
-    Parameters:
-    ---
-        frameNumber: 
-            the number of the frame, 
-        vicsek: VICSEK(class) default: 
-            vicsek model
-    '''
+        Section
+        --
+        >>> step (time) init
+        >>> space init
+        >>> progressbar init
+        >>> file init: folder and hdf5 file 
 
-    P = update(vicsek=vicsek)
-    plt.cla()
-    plt.xlim((0, vicsek.l))
-    plt.ylim((0, vicsek.l))
-    plt.quiver(P[:,0],P[:,1],vicsek.V[:,0],vicsek.V[:,1])
-    # return process
-    pass
+        '''
+
+        # step (time) init
+        #------------
+        # self.simulationTime = 10 # the duration of the simulation, unit: second
+        # self.step = 0.1 # the duration of the step
+        # self.stepNum = int(self.simulationTime/self.step)
+        self.stepNum = 100
+        self.now = 0 # record what the step is now, start from 0~(stepNum-1)
+        
+        # space init
+        #----------------
+        # space for data to be saved
+        self.ThetaSaved = np.zeros((self.n, self.stepNum))
+
+        # progressbar init
+        #------------
+        widgets = ['Progress: ',progressbar.Percentage(), ' ', progressbar.Bar('#'),' ', progressbar.Timer(),
+           ' ', progressbar.ETA(), ' ', progressbar.FileTransferSpeed()]
+        self.pbar = progressbar.ProgressBar(widgets=widgets, max_value=self.stepNum).start()
+        # self.pbar = progressbar.ProgressBar().start()
+
+        #file init
+        #-----------
+        # init folder
+        now = time.localtime()
+        timeStr = time.strftime("%Y-%m-%d_%H-%M-%S",now)
+        self.FolderName = timeStr + '_' + str(self.n) + 'units_' + \
+                    str(self.stepNum) + 'StepNumber_' + \
+                    str(self.yeta) + 'Noise_' + \
+                    str(self.l) + 'size_' + \
+                    str(self.v) + 'speed'
+        self.mypath = os.path.split(__file__)[0]
+        os.chdir(self.mypath)
+        if not os.path.isdir(self.FolderName):
+            os.mkdir(self.FolderName)
+        os.chdir(self.FolderName) 
+        # os.chdir(output_folder) 先不进入，进入后就无法进行多文件的读写操作了最后进行统一的复制
+
+        # init file
+        fileName = "vicsekData.hdf5"
+        self.file = h5py.File(fileName, 'w-')
+
+
+    def data_Save(self):
+        '''
+        save data
+        '''
+        self.file.create_dataset('angleSaved', data=self.ThetaSaved, compression='gzip', compression_opts=9)
+        self.file.close()
+        os.chdir("..")
+        pass
 
 
 
-#================ update data
-#%%
-def update(vicsek: VICSEK):
-    '''
-    update the position.        ref:[2] Emergent Behavior in Flocks
+    def _move(self, frameNumber):
+        '''
+        # WARN!!!!!!!!!!
+        do not touch easily
+        由动画函数自动调用
 
-    Parameters:
-        vicsek: VICSEK(class) default: 
-            vicsek model
-    '''
-    dx = np.subtract.outer(vicsek.P[:, 0], vicsek.P[:, 0])
-    dy = np.subtract.outer(vicsek.P[:, 1], vicsek.P[:, 1]) 
-    distance = np.hypot(dx, dy)
-    # periodic boundary
-    Ax = (distance >= 0) * (distance <= vicsek.r) # >=0是包括自己 
+        Parameters:
+        ---
+            frameNumber: 
+                the number of the frame, 
+        '''
+        # print("\n___framsjlasjldjla",self.now)
+        P, Theta = self.update()
+        self.ThetaSaved[:,self.now] = Theta.reshape((self.n, ))
+        self.pbar.update(self.now+1)
+        self.now +=1
+        plt.cla()
+        plt.xlim((0, self.l))
+        plt.ylim((0, self.l))
+        plt.quiver(P[:,0],P[:,1],self.V[:,0],self.V[:,1])
+        pass
 
-    Ax += (dy > vicsek.l/2) * (np.abs(dx)< vicsek.l/2) * (np.hypot(0-dx,vicsek.l-dy)<vicsek.r)
-    Ax += (dy > vicsek.l/2) * (dx< -vicsek.l/2) * (np.hypot(-vicsek.l-dx,vicsek.l-dy)<vicsek.r)
-    Ax += (dy > vicsek.l/2) * (dx> vicsek.l/2) * (np.hypot(vicsek.l-dx,vicsek.l-dy)<vicsek.r)
-    Ax += (dy < vicsek.l/2) * (dx> vicsek.l/2) * (np.hypot(vicsek.l-dx,0-dy)<vicsek.r)#
-    Ax += Ax.T
 
-    di = np.maximum(Ax.sum(axis=1), 1) #.reshape(vicsek.n,1)
-    Dx = np.diag(di)
-    
-    Lx = Dx-Ax
-    Id = np.identity(vicsek.n)
-    # noise
-    rng = np.random.default_rng()
-    Noises = rng.random((vicsek.n,1))*vicsek.yeta - vicsek.yeta/2
-    
-    vicsek.V = np.matmul((Id - np.matmul(np.linalg.inv(Dx),Lx)),vicsek.V)
-    
-    '''
-    上面的计算变成代码确实有点复杂，但公式还算简单.很多代码是调整矩阵的格式用的
-    ps: “.” 加上运算符表示按元素进行运算
-    V(t+1) = V(t) - Dx^{-1} * Lx * V(t) = (Id - Dx^{-1}* Lx) * V(t)
-    
-    '''
+    #================ update data
 
-    vicsek.V += Noises
-    
-    # speed remains unchanged
-    velocityValue = np.hypot(vicsek.V[:,0],vicsek.V[:,1])
-    vicsek.V[:,0] *= vicsek.v/velocityValue
-    vicsek.V[:,1] *= vicsek.v/velocityValue
-    vicsek.Theta = np.arccos(vicsek.V[:,0]/velocityValue)
+    def update(self):
+        '''
+        # Main logic
+        
+        update the position.        ref:[2] Emergent Behavior in Flocks
+        
+        Parameters:
 
-    vicsek.P  = vicsek.P + vicsek.V * vicsek.step 
-    # print(vicsek.P)
-    # vicsek.V = np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))
-    # vicsek.P = vicsek.P + np.hstack((vicsek.v*np.cos(vicsek.Theta),vicsek.v*np.sin(vicsek.Theta)))*vicsek.step
-    # vicsek.P = vicsek.P % vicsek.l#取余
-    
-    vicsek.P = np.mod(vicsek.P, vicsek.l) # 取余
-    return vicsek.P
+        '''
+        dx = np.subtract.outer(self.P[:, 0], self.P[:, 0])
+        dy = np.subtract.outer(self.P[:, 1], self.P[:, 1]) 
+        distance = np.hypot(dx, dy)
+        # periodic boundary
+        Ax = (distance >= 0) * (distance <= self.r) # >=0是包括自己 
+        #                                                     condition
+        Ax += (dy > self.l/2) * (np.abs(dx)< self.l/2) * (np.hypot(0-dx,self.l-dy)<self.r)
+        Ax += (dy > self.l/2) * (dx< -self.l/2)        * (np.hypot(-self.l-dx,self.l-dy)<self.r)
+        Ax += (dy > self.l/2) * (dx> self.l/2)         * (np.hypot(self.l-dx,self.l-dy)<self.r)
+        Ax += (dy < self.l/2) * (dx> self.l/2)         * (np.hypot(self.l-dx,0-dy)<self.r)#
+        Ax += Ax.T
+        # print(Ax)
+        di = np.maximum(Ax.sum(axis=1), 1) #.reshape(self.n,1)
+        Dx = np.diag(di)
+        # print(Dx)
+        Lx = Dx-Ax
+        Id = np.identity(self.n)
+        #  weight matrix
+        #  Wx is a nonnegative asymmetric matrix whose wij element determines the interaction strength that particle i exerts on particle j
+        Wx= np.ones((self.n,self.n))
+        WxA = Ax * Wx
+
+        # noise
+        rng = np.random.default_rng()
+        Noises = rng.random((self.n,1))*self.yeta - self.yeta/2
+        
+        
+        
+        self.Theta = np.arctan2(np.matmul(WxA,self.V[:,1].reshape(self.n,1)),\
+                                np.matmul(WxA,self.V[:,0].reshape(self.n,1))+0.000000000001) 
+        self.Theta = self.Theta + Noises
+
+        self.Theta = np.mod(self.Theta, 2 * np.pi)
+        '''
+        ps: “.” 加上运算符表示按元素进行运算
+        Theta(t+1) =  <Theta(t)> + noise              
+                                (Wx.*Ax)*Vy(t)                    
+        <Theta(t)> = arctan(  ————————————————————— )         Vx为速度x的分量, Vy同理, epsilon趋于0(防止分母为零)
+                            (Wx.*Ax)*Vx(t)+epsilon
+        
+        '''
+        # print(self.Theta)
+        # speed remains unchanged
+        self.V = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
+        # print(self.V)
+        self.P  = self.P + self.V * 1 # 1 means time
+        # print(self.P) 
+        self.P = np.mod(self.P, self.l) # 取余
+        return self.P, self.Theta
 
 
 #%%
 if __name__ == "__main__":
     #修改参数就修改类的参数就行，这样可以同时跑多个参数
-    vicsek = VICSEK(sizeOfArena= 200.0, number= 400, speed=10, senseRadiu=5, noises=0.6)
-    animate(vicsek)
+    vicsek = VICSEK(sizeOfArena= 100.0, number= 200, speed=.5, senseRadiu=3, noises=.05,name="vicsek_A")
+    vicsek2 = VICSEK(sizeOfArena= 100.0, number= 400, speed=.5, senseRadiu=3, noises=.05,name="vicsek_AA")
+    vicsek.start()
+    ##########
+    vicsek2.start()
 
 
 
 '''
 [1] Novel Type of Phase Transition in a System of Self-Driven Particles
 [2] Emergent Behavior in Flocks
+[3] Modes of information flow in collective cohesion
 '''

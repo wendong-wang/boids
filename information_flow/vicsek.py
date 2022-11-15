fa6a5796fa9cd720cc40756e4179cca4acc25f39
Pang-Jiah <pangjiah@foxmail.com>
2022-11-15 19:06:46 +0800

    2022/11/15 dat: calculate information

--- information_flow/vicsek.py
+++ information_flow/vicsek.py
@@ -1,9 +1,10 @@
-# vicsek model simulation
+# vicsek model simulation [1]
 # Under developing
 # authority: Pang Jiahuan
 # start time: 2022/11/7
-# last time: 2022/11/11
+# last time: 2022/11/15
 # end time: ~
+# python: 3.6
 
 '''
 vicsek model version description:
@@ -12,16 +13,20 @@ vicsek model version description:
 相对与上一版改进的说明：
     取消了惯性，改用速度取平均,加入数据保存,但是这里保存只是考虑了角度值
     结构也大改，将仿真作为类的一个方法，并将仿真数据与模型参数数据进行隔离
+    数据：角度，步数
 '''
 #考虑：
 #   1. 数学计算采用[2]的方式？还是逐个元素进行相应的计算
-#   2. 加速度的表示，注意量纲dimension
+#   2. 加速度的表示，注意量纲dimension (暂时不考虑惯性了)
 #   3. 各种区间边界的开闭确定： 角度， 噪声
-#   4. HDF5数据的结构
+#   4. HDF5数据的结构 (暂不建立group, only dataset)
 #   5. 能否把控时间，而不只是仅仅考虑多少步，simulationTime step（暂时删除）存在的意义
 #   6. 把视频保存放进data_Save()函数
+#   7. 全大写的变量定义为常量，这里希望进行略微的修改(为了步保证变量，后面加入小写)
+#   ·。版本修改3.8->3.6 (numpy 在python3.8 下的一些函数竟然在 python3.6中也好使？虽然代码上没有了相应的提示符 weird)
 
 import numpy as np
+
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
@@ -30,7 +35,7 @@ import os
 import h5py
 import progressbar
 
-class VICSEK():
+class Vicsek():
     '''
     class of Vicsek model, each class is a group of Vicsek model
 
@@ -59,11 +64,11 @@ class VICSEK():
         # n: n units 1x1 个数
         # v: value of the velocities of the units  1x1 速度大小
         # Theta: directions nx1 角度(每个unit运动的不同)
-        # V: velocities of the units nx2
-        # P: matrix of the positions of the units n x 2 x stepNum 位置矩阵
+        # Vo: velocities of the units nx2
+        # Po: matrix of the positions of the units n x 2 x stepNum 位置矩阵
         # r: the radius that an unit can feel 1x1 所感知的半径
         # yeta: the strength of the noises 1x1 噪声
-    大小写规范问题:小写为数值，大写为矩阵
+    大小写规范问题:小写为数值，大写开头为矩阵
 
     '''
     def __init__(self, sizeOfArena: float, number: int, speed: float, senseRadiu: float,noises: float, name: str = "vicsek" ) -> None:
@@ -101,11 +106,11 @@ class VICSEK():
         self.yeta = noises
         # init Position P
         rng = np.random.default_rng()
-        self.P = rng.random((self.n,2))*self.l
+        self.Po = rng.random((self.n,2))*self.l
         # init angle   Theta        0~2*pi but what is 
         self.Theta = rng.random((self.n,1))*2*np.pi 
         # init Velocity  V    
-        self.V = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
+        self.Vo = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
 
         
         pass
@@ -122,7 +127,7 @@ class VICSEK():
         self.simulation_Init() # init file and folder 
 
         fig = plt.figure() # 
-        plt.quiver(self.P[:,0],self.P[:,1],self.V[:,0],self.V[:,1])
+        plt.quiver(self.Po[:,0],self.Po[:,1],self.Vo[:,0],self.Vo[:,1])
         ani = animation.FuncAnimation(fig=fig, func=self._move, frames=self.stepNum-1, interval=20, blit=False, repeat=False) # frams-1是因为frame会传两个参数0
         plt.xlim((0, self.l))
         plt.ylim((0, self.l))
@@ -171,17 +176,17 @@ class VICSEK():
         # init folder
         now = time.localtime()
         timeStr = time.strftime("%Y-%m-%d_%H-%M-%S",now)
-        self.FolderName = timeStr + '_' + str(self.n) + 'units_' + \
+        self.folderName = timeStr + '_' + str(self.n) + 'units_' + \
                     str(self.stepNum) + 'StepNumber_' + \
                     str(self.yeta) + 'Noise_' + \
                     str(self.l) + 'size_' + \
                     str(self.v) + 'speed'
         self.mypath = os.path.split(__file__)[0]
         os.chdir(self.mypath)
-        if not os.path.isdir(self.FolderName):
-            os.mkdir(self.FolderName)
-        os.chdir(self.FolderName) 
-        # os.chdir(output_folder) 先不进入，进入后就无法进行多文件的读写操作了最后进行统一的复制
+        if not os.path.isdir(self.folderName):
+            os.mkdir(self.folderName)
+        os.chdir(self.folderName) 
+
 
         # init file
         fileName = "vicsekData.hdf5"
@@ -192,7 +197,9 @@ class VICSEK():
         '''
         save data
         '''
+        stepNum = np.array([self.stepNum])
         self.file.create_dataset('angleSaved', data=self.ThetaSaved, compression='gzip', compression_opts=9)
+        self.file.create_dataset('stepNum', data=stepNum, compression='gzip', compression_opts=9)
         self.file.close()
         os.chdir("..")
         pass
@@ -211,14 +218,14 @@ class VICSEK():
                 the number of the frame, 
         '''
         # print("\n___framsjlasjldjla",self.now)
-        P, Theta = self.update()
+        Po, Theta = self.update()
         self.ThetaSaved[:,self.now] = Theta.reshape((self.n, ))
         self.pbar.update(self.now+1)
         self.now +=1
         plt.cla()
         plt.xlim((0, self.l))
         plt.ylim((0, self.l))
-        plt.quiver(P[:,0],P[:,1],self.V[:,0],self.V[:,1])
+        plt.quiver(Po[:,0],Po[:,1],self.Vo[:,0],self.Vo[:,1])
         pass
 
 
@@ -233,8 +240,8 @@ class VICSEK():
         Parameters:
 
         '''
-        dx = np.subtract.outer(self.P[:, 0], self.P[:, 0])
-        dy = np.subtract.outer(self.P[:, 1], self.P[:, 1]) 
+        dx = np.subtract.outer(self.Po[:, 0], self.Po[:, 0])
+        dy = np.subtract.outer(self.Po[:, 1], self.Po[:, 1]) 
         distance = np.hypot(dx, dy)
         # periodic boundary
         Ax = (distance >= 0) * (distance <= self.r) # >=0是包括自己 
@@ -261,8 +268,8 @@ class VICSEK():
         
         
         
-        self.Theta = np.arctan2(np.matmul(WxA,self.V[:,1].reshape(self.n,1)),\
-                                np.matmul(WxA,self.V[:,0].reshape(self.n,1))+0.000000000001) 
+        self.Theta = np.arctan2(np.matmul(WxA,self.Vo[:,1].reshape(self.n,1)),\
+                                np.matmul(WxA,self.Vo[:,0].reshape(self.n,1))+0.000000000001) 
         self.Theta = self.Theta + Noises
 
         self.Theta = np.mod(self.Theta, 2 * np.pi)
@@ -276,19 +283,19 @@ class VICSEK():
         '''
         # print(self.Theta)
         # speed remains unchanged
-        self.V = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
-        # print(self.V)
-        self.P  = self.P + self.V * 1 # 1 means time
+        self.Vo = np.hstack((self.v*np.cos(self.Theta),self.v*np.sin(self.Theta)))
+        # print(self.Vo)
+        self.Po  = self.Po + self.Vo * 1 # 1 means time
         # print(self.P) 
-        self.P = np.mod(self.P, self.l) # 取余
-        return self.P, self.Theta
+        self.Po = np.mod(self.Po, self.l) # 取余
+        return self.Po, self.Theta
 
 
 #%%
 if __name__ == "__main__":
-    #修改参数就修改类的参数就行，这样可以同时跑多个参数
-    vicsek = VICSEK(sizeOfArena= 100.0, number= 200, speed=.5, senseRadiu=3, noises=.05,name="vicsek_A")
-    vicsek2 = VICSEK(sizeOfArena= 100.0, number= 400, speed=.5, senseRadiu=3, noises=.05,name="vicsek_AA")
+    #使用类和对象的方式，这样可以同时跑多个参数
+    vicsek = Vicsek(sizeOfArena= 100.0, number= 200, speed=.5, senseRadiu=3, noises=.05,name="vicsek_A")
+    vicsek2 = Vicsek(sizeOfArena= 100.0, number= 400, speed=.5, senseRadiu=3, noises=.05,name="vicsek_AA")
     vicsek.start()
     ##########
     vicsek2.start()
@@ -296,7 +303,9 @@ if __name__ == "__main__":
 
 
 '''
-[1] Novel Type of Phase Transition in a System of Self-Driven Particles
-[2] Emergent Behavior in Flocks
-[3] Modes of information flow in collective cohesion
+[1] Vicsek, T., Czirók, A., Ben-Jacob, E., Cohen, I. & Shochet, O. Novel Type of Phase Transition in a System of Self-Driven Particles. Phys. Rev. Lett. 75, 1226-1229 (1995).
+[2] Cucker, F. & Smale, S. Emergent Behavior in Flocks. IEEE Trans. Automat. Contr. 52, 852-862 (2007).
+[3] Sattari, S. et al. Modes of information flow in collective cohesion. SCIENCE ADVANCES 14 (2022).
+
+
 '''
--- information_flow/vicsek_data_processing.py
+++ information_flow/vicsek_data_processing.py
@@ -2,26 +2,28 @@
 # Under developing
 # authority: Pang Jiahuan
 # start time: 2022/10/30
-# last time: 2022/11/11
+# last time: 2022/11/15
 # end time: ~
 #information flow: mutual information; 
 #                  time-delayed mutual information; 
 #                  transfer entropy; 
 #                  shared, intrinsic, syn.. entropy.
-
+# python: 3.6
 '''
 version description:
-    读取数据,将H5PY常用操作包装成一个class
-    ！！！现在这个文件还不可以运行
+    初步实现所要熵的计算,intrinsic information flow的正确性还有待考究
 '''
 #考虑：
-#   1. 信息流处理类的建立
-
+#   ·. 取和不为1
+#   ·.intrinsic information flow
 import h5py
 import numpy as np
 import os
 
-class H5PY_PROCESSOR():
+import itertools as it #排列组合用
+import dit      
+
+class H5PY_Processor():
     '''
     A Class for basic HDF5 file operation
 
@@ -48,7 +50,7 @@ class H5PY_PROCESSOR():
         pass
     def search_Deep(self,path: str,name:str = '/') -> None:
         '''
-        Using depth-first algorithm to draw the file structure of HDF5 group. 
+        Use depth-first algorithm to draw the file structure of HDF5 groups. 
 
         Parameters
         -------
@@ -59,7 +61,11 @@ class H5PY_PROCESSOR():
         
         Warning
         ----------
-        this function now is under development and is not able to vetify the correctness of the input parameters. 
+        this function now is under development and is not able to vertify the correctness of the input parameters.
+
+        Examples:
+        ---
+            self.search_Deep(path = '/')
         
         '''
         dog = self.f[path] # a group or dataset
@@ -84,31 +90,182 @@ class H5PY_PROCESSOR():
             # print(dog)
             print(name,"[D]", np.shape(dog))#没有长度的dataset可能会被看作是group，因此len()失效
 
-class INFORMATION_PROCESSOR():
-    def __init__(self, Theta) -> None:
+class Information_Processor():
+    def __init__(self, Theta, stepNum) -> None:
         self.Theta = Theta
+        self.stepNum = stepNum
+        self.discretize()
+        #init alphabet
+        self.alphabet={}
+        for e in it.product('012345', repeat=3):
+            a = ''.join(e)
+            self.alphabet[a] = 0.0   
+        # print(self.alphabet)  
+        self.count_Distribution(x=0,y=1)
+        self.calculate_Information()
         pass
+
+    def discretize(self):
+        '''
+        discretize the angle set (self.Theta (0~2*pi )) into 6 parts
+        0 1 2 3 4 5  
+
+        for example: [0°,60°) -> 0; [60°,120°) -> 1 ...
+
+        '''
+        Theta = self.Theta
+        Theta = np.mod(Theta,2*np.pi)#将2*pi变为0
+        Theta = np.floor_divide(Theta, np.pi/3)
+        self.bins = Theta 
+        pass
+
+
+    def count_Distribution(self, x:int, y:int):
+        '''
+        count the occurrences of  (x(t), y(t), y(t+τ)) 
+            x affect y. For convenience, let X, Y, and Z denote x(t), y(t), y(t+τ)seperately.
+
+        Parameters
+        --
+        >>> x,y: int
+            the index of vairable
+
+        
+        '''
+        # count
+        for i in range(self.stepNum-1):
+            index = str(int(self.bins[i][x])) + str(int(self.bins[i][y])) + str(int(self.bins[i+1][y]))
+            # print(index)
+            self.alphabet[index] += 1/(self.stepNum-1)
+
+        alphabetKeys  = list(self.alphabet.keys()) # seperate the keys form dict
+        alphabetValue = list(self.alphabet.values()) # seperate the value from dict
+
+        self.XYZ = dit.Distribution(alphabetKeys, alphabetValue)
+        self.XYZ.set_rv_names('XYZ')
+        # print(self.XYZ)
+        pass
+
+
+
+        
+
     def mutual_Information(self):
-        pass
+        '''
+        calculate the mutual information of x(t) and y(t)
+
+        Return
+        ---
+            mI: mutual information of x(t) and y(t)
+        '''
+        
+        mI = dit.shannon.mutual_information(self.XYZ,'X','Y')
+        # print(mutual_Information)
+        return mI
+        
     def time_Delayed_Mutual_Information(self):
-        pass
+        '''
+        calculate time delayed mutual information (TDMI)
+        
+        Return
+        ---
+            tMDI: TMDI
+        '''
+        tMDI = dit.shannon.mutual_information(self.XYZ,'X','Z')
+        return tMDI
+
     def transfer_Entropy(self):
-        pass
+        '''
+        calculate  transfer_Entropy (TE)
+        
+        Return
+        ---
+            tE: TE
+        '''
+        # tE = dit.multivariate.coinformation(self.XYZ, rvs = 'XZ', crvs = 'Y')
+        tE = dit.multivariate.coinformation(self.XYZ, 'XZ','Y')
+        return tE
+
+
     def intrinsic_Information_Flow(self):
-        pass
-    def shared_Information_Flow(self):
-        pass
-    def synergistic_Information_Flow(self):
-        pass
+        '''
+        calculate time intrinsic_Information_Flow (IIF)
+        
+        Return
+        ---
+            iIF: IIF
+        '''
+        iIF = dit.multivariate.secret_key_agreement.intrinsic_mutual_information(self.XYZ, 'XZ','Y')
+        return iIF
+
+    def shared_Information_Flow(self, tE, iIF):
+        '''
+        calculate time shared_Information_Flow (SHIF)
+        
+        Parameters
+        ---
+        >>> tE: TE
+        >>> iIF: IIF
+
+        Return
+        ---
+            sHIF: SHIF
+        '''
+        sHIF = tE - iIF
+        return sHIF
 
 
+
+    def synergistic_Information_Flow(self, tDMI, iIF):
+        '''
+        calculate time synergistic_Information_Flow (SYIF)
+        
+        Parameters
+        ---
+        >>> tDMI: TDMI
+        >>> iIF: IIF
+
+        Return
+        ---
+            sYIF: SYIF
+        '''
+        sYIF = tDMI - iIF
+        return sYIF
+
+    def calculate_Information(self):
+        '''
+        calculate all the information we need
+        '''
+        self.mI = self.mutual_Information()
+        self.tDMI = self.time_Delayed_Mutual_Information()
+        self.tE = self.transfer_Entropy()
+        self.iIF = self.intrinsic_Information_Flow()
+        self.sHIF = self.shared_Information_Flow(tE= self.tE, iIF= self.iIF)
+        self.sYIF = self.synergistic_Information_Flow(tDMI=self.tDMI, iIF=self.iIF)
+        pass
 
 if __name__ == "__main__":
+    # enter the absolute path of this python file
     mypath = os.path.split(__file__)[0]
     os.chdir(mypath)
-    #
-    folderName = "2022-11-11_22-28-47_400units_100StepNumber_0.05Noise_100.0size_0.5speed" #想要处理的数据名称
+
+    # enter the folder containing the data
+    folderName = "2022-11-14_22-11-36_200units_100StepNumber_0.05Noise_100.0size_0.5speed" #想要处理的所在数据名称
     os.chdir(folderName)
-    # f = H5PY_PROCESSOR("h5py_example.hdf5",'r')
+    # get the file
+    f = H5PY_Processor("vicsekData.hdf5","r")
     # f.search_Deep("/")
-    # f.close()
+    processor = Information_Processor(Theta= f.f["angleSaved"][:,:],stepNum=f.f['stepNum'][0])
+    f.close()
+
+    print("multual_Information:",processor.mI)
+    print("time_Delayed_Mutual_Information:",processor.tDMI)
+    print("transfer_Entropy:",processor.tE)
+    print("intrinsic_Information_Flow:",processor.iIF)
+    print("shared_Information_Flow:",processor.sHIF)
+    print("synergistic_Information_Flow:",processor.sYIF)
+
+'''
+reference:
+1. G. James, R., J. Ellison, C. & P. Crutchfield, J. dit: a Python package for discrete information theory. JOSS 3, 738 (2018).
+'''
